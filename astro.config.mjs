b55c86c26db21c472262425de46f249786fd5c34
Pomelo-Xsl <xsl20020718@gmail.com>
2024-12-01 14:41:37 +0800

    修复了一个页面数超过4页无法跳转的bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 // https://astro.build/config
 export default defineConfig({
   site: "https://Xiesl.pages.dev",
-  base:"/blog/",
+  base:"/blog",
   style: {
     scss: {
       includePaths: ["./src/styles"],
